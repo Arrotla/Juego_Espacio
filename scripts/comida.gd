785ad8582460a0660b3b87e185a4afc31d9e551b
Arrotla <arturo.arrotla@gmail.com>
2021-09-02 16:18:41 -0500

    Movimiento personaje, Modificación tiempo de los medidores

--- scripts/comida.gd
+++ scripts/comida.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
 func _ready():
-	yield(get_tree().create_timer(80), "timeout")
+	yield(get_tree().create_timer(60), "timeout")
 	$"Comida_1".hide()
 	$"Comida_2".show()
-	yield(get_tree().create_timer(80), "timeout")
+	yield(get_tree().create_timer(50), "timeout")
 	$"Comida_2".hide()
 	$"Comida_3".show()
-	yield(get_tree().create_timer(80), "timeout")
+	yield(get_tree().create_timer(40), "timeout")
 	$"Comida_3".hide()
 	$"Comida_4".show()
-	yield(get_tree().create_timer(80), "timeout")
+	yield(get_tree().create_timer(40), "timeout")
 	$"Comida_4".hide()
 	$"Comida_5".show()
-	yield(get_tree().create_timer(80), "timeout")
+	yield(get_tree().create_timer(40), "timeout")
 	$"Comida_5".hide()
