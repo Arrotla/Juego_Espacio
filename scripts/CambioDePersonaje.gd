24d4d898067784446ddaf1d9580386dc30ea384c
robluxxdyt <abdiasbrunogf@gmail.com>
2021-09-02 18:58:01 -0300

    cambio de personaje(en proceso)

---
+++ scripts/CambioDePersonaje.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+	if Input.is_action_pressed("papa"):
+		pass
+		
+		
+	if Input.is_action_pressed("mama"):
+		pass
+		
+		
+	if Input.is_action_pressed("hijo"):
+		pass
