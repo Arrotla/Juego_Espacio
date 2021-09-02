01a0cb1ff6bedc147b626aad35f1f1f1d03c8b39
robluxxdyt <abdiasbrunogf@gmail.com>
2021-09-02 16:38:50 -0300

    Update comida.gd

--- scripts/comida.gd
+++ scripts/comida.gd
@@ -1,18 +1,17 @@
 extends Node2D
 
 func _ready():
-	Input.is_action_just_pressed("ui_down")
-	yield(get_tree().create_timer(120), "timeout")
+	yield(get_tree().create_timer(60), "timeout")
 	$"Comida_1".hide()
 	$"Comida_2".show()
-	yield(get_tree().create_timer(120), "timeout")
+	yield(get_tree().create_timer(60), "timeout")
 	$"Comida_2".hide()
 	$"Comida_3".show()
-	yield(get_tree().create_timer(120), "timeout")
+	yield(get_tree().create_timer(60), "timeout")
 	$"Comida_3".hide()
 	$"Comida_4".show()
-	yield(get_tree().create_timer(120), "timeout")
+	yield(get_tree().create_timer(60), "timeout")
 	$"Comida_4".hide()
 	$"Comida_5".show()
-	yield(get_tree().create_timer(120), "timeout")
+	yield(get_tree().create_timer(60), "timeout")
 	$"Comida_5".hide()
